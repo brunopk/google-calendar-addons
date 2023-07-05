84c175b05544b3a265db87e9cac70fae721c94fb
Bruno Piaggio <brunopiaggiok@gmail.com>
2023-07-04 22:06:47 -0300

    Improve createAllDayEvent function

--- CalendarUtils.ts
+++ CalendarUtils.ts
@@ -1,6 +1,15 @@
-function createAllDayEvent(calendarId: string, title: string, day: Date, description: string) {
+function createAllDayEvent(calendarId: string, title: string, date: Date, description: string) {
+  const endDate = plusHours(date, 24)
+
+  console.info(`Adding all day event for ${date}`)
+
   return Calendar.Events?.insert(
-    { summary: title, description, start: { date: toLocalDateString(day) }, end: { date: toLocalDateString(day) } },
+    {
+      summary: title,
+      description,
+      start: { date: toLocalDateString(date) },
+      end: { date: toLocalDateString(endDate) }
+    },
     calendarId
   )
 }
