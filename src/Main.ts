f95fa05fa694aaf9ba493fdd0ecf889c0e9ae5fc
Bruno Piaggio <brunopiaggiok@gmail.com>
2023-08-28 12:54:45 -0300

    Sorting task by due date for mail

--- src/Main.ts
+++ src/Main.ts
@@ -21,6 +21,7 @@ function sendMailForPendingTasks() {
   const tasks = taskLists
     .map((taskList) => listPendingTasks(taskList))
     .reduce((previousValue, currentValue) => previousValue.concat(currentValue), [])
+    .sort((taskA, taskB) => new Date(taskA.due as string).getTime() - new Date(taskB.due as string).getTime())
   const htmlBody = formatAsHtml(tasks)
   MailApp.sendEmail(MAIL_RECIPIENT, MAIL_SUBJECT, "", { htmlBody })
 }
